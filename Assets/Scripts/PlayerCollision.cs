c05a3eae591cddf3ff0dccb9e145c54aa4171ff0
Skoftig <saraderooij1@gmail.com>
2019-02-25 14:28:29 +0100

    initial commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    //public GameManager _manager;
+    
+
+    private void OnCollisionEnter (Collision col)
+    {
+        if (col.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            //_manager.EndGame();
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
